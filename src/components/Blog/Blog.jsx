5255f061eb9223daf8a0bd39b1b34824625006fb
rifatwebcraft <cmohammadrifat@gmail.com>
2024-03-17 03:30:50 +0600

    git commit again and again

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
     console.log(blog);
@@ -8,5 +8,9 @@ const Blog = ({blog}) => {
         </div>
     );
 };
+
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
+}
 
 export default Blog;
