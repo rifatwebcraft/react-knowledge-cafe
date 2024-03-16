a21e3467214b80c4c7662f0014c6ca8e17081815
rifatwebcraft <cmohammadrifat@gmail.com>
2024-03-17 03:29:44 +0600

    proptypes set now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
   
@@ -8,7 +9,11 @@ function App() {
   return (
     <>
     <Header></Header>
+    <div className='md:flex'>
     <Blogs></Blogs>
+    <Bookmarks></Bookmarks>
+    </div>
+    
    
     </>
   )
